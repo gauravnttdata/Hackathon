4adaa8b052be85d979ee603ee9d840bd7b455b5a
077962 <navdeep.singh@nttdata.com>
2013-09-13 21:38:38 -0700

    -a

--- unixmen/protected/config/main.php
+++ unixmen/protected/config/main.php
@@ -10,7 +10,7 @@ return array(
 	'name'=>'Hackathon',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>array('log','bootstrap',),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -20,14 +20,17 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'Enter Your Password Here',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+			'generatorPaths' => array(
+					'bootstrap.gii'
+			),
 		),
-		*/
+		
 	),
 
 	// application components
@@ -79,6 +82,11 @@ return array(
 				*/
 			),
 		),
+		'bootstrap' => array(
+				'class' => 'ext.bootstrap.components.Bootstrap',
+				'responsiveCss' => true,
+		),
+		'theme'=>'bootstrap',
 	),
 
 	// application-level parameters that can be accessed
