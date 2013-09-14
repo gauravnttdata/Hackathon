1a41d97e67cc1d5834d3b6f474c088c1e06037d3
077962 <navdeep.singh@nttdata.com>
2013-09-14 01:34:49 -0700

    -a

--- unixmen/protected/views/site/index.php
+++ unixmen/protected/views/site/index.php
@@ -63,7 +63,7 @@ if(isset($_GET['celeb'])){
 ?>	
 <div class="bootstrap-widget">
 	<div class="bootstrap-widget-header">
-		<i class="icon-home"></i><h3>NTTDATAAmericas</h3>
+		<i class="icon-home"></i><h3>@<?php echo $_GET['celeb'];?></h3>
 	</div>
 	<div id="yw1" class="bootstrap-widget-content"><?php echo $this->renderPartial('twitterrender');?></div>
 </div>
