8cffd6026f556c876a424225dc521848ecedf2a1
077962 <navdeep.singh@nttdata.com>
2013-09-14 00:07:34 -0700

    -a

--- unixmen/protected/views/site/index.php
+++ unixmen/protected/views/site/index.php
@@ -66,7 +66,7 @@ if(isset($_GET['celeb'])){
 	    'proxyController' => $this->createUrl('site/get_tweets'), // You need to specify this!
 	    'username' => array($_GET['celeb']), // as you can see you can add an array of usernames
 	    'cssFile' => false, // if you don't want the default CSS file
-	    //'cssFile'=>Yii::app()->theme->baseUrl.'/css/tweet-master.css', // customize your twitter css file
+	    'cssFile'=>Yii::app()->theme->baseUrl.'/css/tweet-master.css', // customize your twitter css file
 	    'options' => array(
 	        'avatar_size' => 32,
 	        'template' => '{user} {text} - {time} - {reply_action} - {retweet_action} - {favorite_action}',
