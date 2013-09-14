1c9d6a9b76d97e977fcd1374fb8f9325afc2e04e
077962 <navdeep.singh@nttdata.com>
2013-09-14 00:39:30 -0700

    -a

--- unixmen/protected/views/site/index.php
+++ unixmen/protected/views/site/index.php
@@ -60,19 +60,12 @@ $this->widget('bootstrap.widgets.TbMenu', array(
 <div style="float: left; width: 930px;">
 <?php
 if(isset($_GET['celeb'])){
-	$this->widget('ext.yii-new-tweet-master.Tweets', array(
-	    'id' => 'twitter-feed',
-	    'csrfToken' => true, // set this to true if you enabled CSRF validation
-	    'proxyController' => $this->createUrl('site/get_tweets'), // You need to specify this!
-	    'username' => array($_GET['celeb']), // as you can see you can add an array of usernames
-	    'cssFile' => false, // if you don't want the default CSS file
-	    //'cssFile'=>Yii::app()->theme->baseUrl.'/css/tweet-master.css', // customize your twitter css file
-	    'options' => array(
-	        'avatar_size' => 32,
-	        'template' => '{user} {text} - {time} - {reply_action} - {retweet_action} - {favorite_action}',
-	        'count' => 12
-	    )
+	$this->widget('bootstrap.widgets.TbBox', array(
+			'title' => 'Basic Box',
+			'headerIcon' => 'icon-home',
+			'content' => $this->renderPartial('twitterrender')
 	));
+	
 }else{
 	
 
